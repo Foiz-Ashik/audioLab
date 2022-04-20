5cd628e2492af947965e8be3937cc44e0df991a1
Foiz-Ashik <77127829+Foiz-Ashik@users.noreply.github.com>
2022-04-19 21:22:45 -0400

    Initial commit

---
+++ index.js
@@ -0,0 +1,43 @@
+var cat = document.getElementById("cat"); 
+var cow = document.getElementById("cow");
+var dog = document.getElementById("dog");
+var horn = document.getElementById("horn");
+var horse = document.getElementById("horse"); 
+
+function playCatudio() { 
+  cat.play(); 
+}
+function pauseCatAudio() { 
+    cat.pause(); 
+  } 
+
+function playCowAudio() { 
+  cow.play(); 
+} 
+function pauseCowAudio() { 
+    cow.pause(); 
+  }
+
+function playDogAudio() { 
+  dog.play(); 
+}
+function pauseDogAudio() { 
+    dog.pause(); 
+  }
+
+function playHornAudio() { 
+  horn.play(); 
+}
+
+function pauseHornAudio() { 
+    horn.pause(); 
+  }
+
+function playHorseAudio() { 
+  horse.play(); 
+}
+
+
+function pauseHorseAudio() { 
+  horse.pause(); 
+}
